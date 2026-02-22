0569cc833cdea208d0e8a15b25748a59eb113edd
keup <yyeet62425@gmail.com>
2026-02-22 18:37:20 +0000

    RGB triangle

--- src/main.cc
+++ src/main.cc
@@ -34,14 +34,10 @@ int main(int argc, char *argv[]) {
     }
 
     float points[] = {
-        0.0f,  0.5f,
-        0.0f, /// xyz of first point
-
-        0.5f,  -0.5f,
-        0.0f, /// xyz of second point
-
-        -0.5f, -0.5f,
-        0.0f, /// xyz of third point
+        // positions          // colors
+        0.0f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, // top -- red
+        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right -- green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // bottom left -- blue
     };
 
     GLuint vbo, vao;
@@ -52,9 +48,13 @@ int main(int argc, char *argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
                           (void *)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
 
